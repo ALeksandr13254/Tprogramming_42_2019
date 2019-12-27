cf7ec5123666caaae1a4b7071e9f46abba336098
Александр <@DESKTOP-6G1MCJ8>
2019-12-28 00:26:11 +0300

    Сделал!

---
+++ RPG/MainRPG.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RPG
+{
+    public class MainRPG
+    {
+        public static void Main(string[] args)
+        {
+            int a = 1;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(a);
+        }
+    }
+}
